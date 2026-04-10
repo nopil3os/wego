85c61203bc399a9ef3450ed68b52e660cb51d175
nopil3os <nopil3os>
2026-04-10 16:44:33 +0200

    remove unit conversation as it is now in km/m2 = mm/m2

--- backends/smhi.go
+++ backends/smhi.go
@@ -49,7 +49,6 @@ type smhiCondition struct {
 }
 
 const (
-	// Ensure this URL is still valid for the new API version. Left unchanged based on prompt context.
 	//smhiWuri = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/%s/lat/%s/data.json"
 	smhiWuri = "https://opendata-download-metfcst.smhi.se/api/category/snow1g/version/1/geotype/point/lon/%s/lat/%s/data.json"
 )
@@ -196,7 +195,7 @@ func (c *smhiConfig) parsePrediction(prediction *smhiTimeSeries) (cnd iface.Cond
 	}
 	cnd.Time = ts
 
-	precip := float32(prediction.Data.PrecipitationAmountMean / 1000) // Convert mm/h to m/h
+	precip := float32(prediction.Data.PrecipitationAmountMean) // no need to convert this anymore
 	cnd.PrecipM = &precip
 
 	vis := float32(prediction.Data.VisibilityInAir * 1000) // Convert km to m
